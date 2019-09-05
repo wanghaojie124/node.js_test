8c773389178dd49b886528c2c1099d6cab1093b8
wanghaojie124 <593164063@qq.com>
2019-09-05 17:55:40 +0800

    2

--- google-sheet-node/spreadsheet.js
+++ google-sheet-node/spreadsheet.js
@@ -5,10 +5,18 @@ const creds = require('./client_secret.json');
 
 async function accessSpreadsheet(){
 	const doc = new GoogleSpreadsheet('1fv3v1RDvcDPAYV0kULINf2W7TiAT85RqYm4Oy6FD5KE');
-	await promisify(doc.useServicAccountAuth)(creds);
+	await promisify(doc.useServiceAccountAuth)(creds);
 	const info = await promisify(doc.getInfo)();
 	const sheet = info.worksheets[0];
 	console.log('title: ${ sheet.title }, Rows: ${ sheet.rowcount }');
+	
+
+	const rows = {
+		album_id: 1,
+		username: 'John',
+		title: 'Happy weekend'
+	}
+	await promisify(sheet.addrow)(row);
 }
 
 accessSpreadsheet();
